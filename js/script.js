6365dd46c78dff861970f65bd8353eb5e2809f6b
Gurmis <gurmooo@gmail.com>
2022-02-23 16:32:50 +0100

    initial files ADDED

---
+++ js/script.js
@@ -0,0 +1,28 @@
+String.prototype.capitalize = function() { return this.charAt(0).toUpperCase() + this.slice(1);};
+(function($) {
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+})(jQuery);
+
+
+
